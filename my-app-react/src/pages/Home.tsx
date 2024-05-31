c7f8caf059486c8a286b98d2137c9bef3fa72b58
Iris-23 <2545237915@qq.com>
2024-05-31 16:15:34 +0800

    删除produce 修改数据流问题

--- my-app-react/src/pages/Home.tsx
+++ my-app-react/src/pages/Home.tsx
@@ -13,7 +13,6 @@ const Home: FC = () => {
     <div className={styles.container}>
       <div className={styles.info}>
         <Title>学科|专业事项数据采集平台</Title>
-        {/* <Paragraph>已累计创建问卷 100 份, 发布问卷 90份, 收到答复 980份</Paragraph> */}
         <div>
           <Button type="primary" onClick={() => nav(MANAGE_INDEX_PATHNAME)}>开始使用</Button>
         </div>
