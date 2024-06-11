b6914afdb43d796ac1469eb12b93945d203e7d99
Iris-23 <2545237915@qq.com>
2024-06-11 18:33:07 +0800

    重填覆盖原有数据

--- my-app-express/controller/Answer.js
+++ my-app-express/controller/Answer.js
@@ -1,33 +1,25 @@
-
 const { SuccessModel } = require('../utils/resModel')
-const { createAnswerData } = require('../db/models/Answer')
+const { createAnswerData, Answer } = require('../db/models/Answer')
 
 const answer = async (req, res, next) => {
+ const { answer } = req.body;
+ console.log('Request body:', req.body);
+ const { questionId, username, answerList } = answer;
 
-  const { answer } = req.body
-  console.log('Request body:', req.body);
-  const { questionId, username, answerList } = answer
+try {
+ const existingAnswer = await Answer.findOneAndUpdate(
+ { questionId, username },
+ { answerList },
+ { new: true, upsert: true } // new: true 返回更新后的文档，upsert: true 如果没有找到则创建一个新文档
+ );
 
-  const answerData = await createAnswerData({ questionId, username, answerList })
-
-  return res.json(new SuccessModel({ id: answerData._id.toString() }))
-}
-
-// const answer = async (req, res) => {
-//   const { questionId, username, answerList } = req.body; // 确保从请求体中获取这些数据
-
-//   if (!username ) {
-//     return res.status(400).json({ error: 'username is required' });
-//   }
-
-//   try {
-//     const answerData = await createAnswerData({ questionId, username, answerList });
-//     return res.status(201).json(answerData);
-//   } catch (error) {
-//     return res.status(500).json({ error: error.message });
-//   }
-// };
+ return res.json(new SuccessModel({ id: existingAnswer._id.toString() }));
+ } catch (error) {
+ console.error('Failed to create or update answer:', error);
+ return res.status(500).json({ error: error.message });
+ }
+};
 
 module.exports = {
-  answer
+  answer
 };
